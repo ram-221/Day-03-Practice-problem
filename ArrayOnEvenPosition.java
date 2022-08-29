ef973249c3cf63661e7a97a3a80e71eab52dd07a
ram-221 <rameshmalli857.com>
2022-08-29 19:46:24 +0530

    Print the Element of an Array on Even Position

---
+++ ArrayOnEvenPosition.java
@@ -0,0 +1,16 @@
+package com.bridgelabday03;
+
+public class ArrayOnEvenPosition {
+
+	public static void main(String arg[]) {
+		int array[] =  {1,2,3,4,5,6,7,8,9,10,11,12};
+		for (int index = 0; index<array.length; index++) {
+			System.out.print(array[index]+" ");
+		}
+		System.out.println();
+		System.out.println("Array On Even Position");
+		for(int index = 1; index<array.length; index = index+2) {
+			System.out.print(array[index]+" ");
+		}
+	}
+}
