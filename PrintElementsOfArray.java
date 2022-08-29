cf4f2e374f297acacaec39223a56d0763d52b6ad
ram-221 <rameshmalli857.com>
2022-08-29 19:40:16 +0530

    Print the elements of an Array

---
+++ PrintElementsOfArray.java
@@ -0,0 +1,12 @@
+package com.bridgelabday03;
+
+public class PrintElementsOfArray {
+
+	public static void main(String arg[]) {
+		int a[] =new int[] {15,30,45,60,75};
+		for(int x=0;x<a.length;x++) {
+			System.out.println(a[x] +" ");
+		}
+		System.out.println("this are elements of aray");
+	}
+}
